450b2aa7b1c4bd195ce5bab302ff308a9392bef1
Nat's Mac <natsmac@Natss-MacBook-Air.local>
2018-08-12 11:03:02 -0400

    add mongodb schema for moves. pass moves object to ejs index for forEach loop.  add logic for seeding DB with moves

--- app.js
+++ app.js
@@ -3,8 +3,55 @@ var app = express();
 var bodyParser       = require("body-parser");
 var mongoose         = require("mongoose");
 
+// APP CONFIG
+mongoose.connect("mongodb://localhost/chess_eos");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// MONGOOSE/MODEL CONFIG
+var moveSchema = new mongoose.Schema({
+    player: String,
+    move: String,
+    created: {type: Date, default: Date.now()}
+});
+
+var Move = mongoose.model("Move", moveSchema);
+
+var data = [
+    {
+        player: "Cloud's rest", 
+        move: "RE1"
+    },
+    {
+        player: "Cloud's rest", 
+        move: "RE4"
+    },
+    {
+        player: "Cloud's rest", 
+        move: "RE6"
+    }
+]
+
+function seedDB(){
+    // Remove all campgrounds
+    Move.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed moves!");
+        data.forEach(function(seed){
+            Move.create(seed, function(err, move){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a move");
+                }; 
+            });
+        });
+    });
+};
+
+seedDB()
 
 // SETUP RESOURCES TO BE USED
 app.use("/js", express.static(__dirname + '/js'));
@@ -12,9 +59,22 @@ app.use("/css", express.static(__dirname + '/css'));
 app.use("/img/chesspieces/wikipedia", express.static(__dirname + '/img/chesspieces/wikipedia'));
 app.use("/stylesheets", express.static(__dirname + '/stylesheets'));
 
-// ROUTES
+
+
+// OLD INDEX ROUTE
+// app.get("/", function(req, res) {
+//     res.render("index");
+// });
+
+// NEW INDEX ROUTE
 app.get("/", function(req, res) {
-    res.render("index");
+    Move.find({}, function(err, allMoves){
+        if(err){
+            console.log(err);
+        } else {
+           res.render("index",{moves : allMoves});
+        }
+     });
 });
 
 // SERVER PROCESS
