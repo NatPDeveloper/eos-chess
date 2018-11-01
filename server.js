07bcd8896126b295d9deef8777e573d16d0ba970
natpdeveloper <natanaelprudhomme@DESKTOP-HV680VC.localdomain>
2018-10-31 21:19:39 -0400

    changed server file

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-// const http = require('http');
+const http = require('http');
 const app = require('./app');
 
-const host = '0.0.0.0';
-
-const port = process.env.PORT || 5000; // sets port and defaults to 5000
+const port = (process.env.PORT || 5000); // sets port and defaults to 5000
 
 // const server = http.createServer(app);
 
-// server.listen(port, host);
-
-app.listen(port, host, function() {
-    console.log("Server started.......");
-});
+server.listen(port);
