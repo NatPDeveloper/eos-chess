e72c334b7d3139c3c349847a9bcac79daba62e26
natpdeveloper <natanaelprudhomme@DESKTOP-HV680VC.localdomain>
2018-10-31 21:10:59 -0400

    add 0.0.0.0 as host

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3526; // sets port and defaults to 3000
+const host = '0.0.0.0';
+
+const port = process.env.PORT || 5000; // sets port and defaults to 5000
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, host);
