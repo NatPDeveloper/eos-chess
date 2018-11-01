cae33d59ea9a0355d352cbfd36c2d5f013eaec33
natpdeveloper <natanaelprudhomme@DESKTOP-HV680VC.localdomain>
2018-10-31 21:34:03 -0400

    t1

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require('http');
 const app = require('./app');
 
-// const port = (); // sets port and defaults to 5000
+// const port = (process.env.PORT || 5000); // sets port and defaults to 5000
 
 // const server = http.createServer(app);
 
-server.listen((process.env.PORT || 5000), (process.env.YOUR_HOST || '0.0.0.0'));
+// server.listen(port);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
